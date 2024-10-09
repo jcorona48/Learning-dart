743537126055b6d062625ef43ca4f54ba961c9f9
Joan Corona <joaneliasc86@gmail.com>
2024-10-09 03:29:59 -0400

    refactor: 💄 Move docs funtion to another file

---
+++ docs.dart
@@ -0,0 +1,68 @@
+import 'utils.dart' show printDivider, printTitle; // Import file utils.dart
+import 'Classes/Person.dart' show Person; // Import class Person from file Person.dart
+
+void printDocumentation() {
+  printTitle('Dart Basic Syntax');
+  printDivider();
+  variableDeclaration();
+  objectDeclaration();
+  conditionalStatement(10, 20);
+  conditionalStatement(20, 10);
+  loopStatement();
+  listDeclaration();
+}
+
+// Function declaration
+void variableDeclaration() {
+  printTitle('Variable Declaration');
+  int? num1; // Variable declaration, allow null
+  int num2 = 5; // Variable declaration, not allow null
+  const int num3 = 20;
+  print('Number 1: ${num1}'); // ${} is used to print the value of a variable
+  print('Number 2: ${num2}');
+  print('Number 3: ${num3}');
+  num1 = 10;
+  //num3 = 30; // ❌ Error: Constant variables can't be assigned a value.
+  print('Number 1 (Rewrite): ${num1}');
+  printDivider();
+}
+
+void objectDeclaration() {
+  printTitle('Object Declaration');
+  Person person = Person(name: 'John', age: 25);
+  person.showInfo();
+  printDivider();
+}
+
+void conditionalStatement(int num1, int num2) {
+  printTitle('Conditional Statement');
+  if (num1 > num2) {
+    print('Number 1 is greater than Number 2');
+  } else {
+    print('Number 1 is less than Number 2');
+  }
+  printDivider();
+}
+
+void loopStatement() {
+  printTitle('Loop Statement');
+  for (int i = 0; i < 5; i++) {
+    print('Index: ${i+1}');
+  }
+  printDivider();
+}
+
+void listDeclaration() {
+  printTitle('List Declaration');
+  List<int> numbers = [1, 2, 3, 4, 5];
+  for (int number in numbers) {
+    print('Number: ${number}');
+  }
+  printDivider();
+}
+
+/* Notes: 
+Why can't use class into function?
+Because the class is a global declaration, it can be used in any function.
+*/
+
